b0306d7897d28a0a07665f036d98e0dcfb758b22
toncy11 <ajagodic1990@gmail.com>
2020-05-26 22:03:42 +0200

    tests Updates

--- src/test/java/com/opneclassrooms/paymybuddy/BaseDataTest.java
+++ src/test/java/com/opneclassrooms/paymybuddy/BaseDataTest.java
@@ -7,14 +7,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import org.junit.Before;
-import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.openclassrooms.paymybuddy.repository.AccountRepository;
+import com.openclassrooms.paymybuddy.repository.UserRepository;
 
 public class BaseDataTest {
 
@@ -22,6 +22,12 @@ public class BaseDataTest {
 
 	@Autowired
 	private WebApplicationContext webContext;
+	
+	@Autowired 
+	private UserRepository userRepository;
+	
+	@Autowired 
+	private AccountRepository accountRepository;
 
 	@Before
 	public void setupMockmvc() {
@@ -35,9 +41,11 @@ public class BaseDataTest {
 		mockMvc.perform(post("/signup").param("email", "test@mail.com").param("password", "1234"))
 				.andExpect(view().name("user/signup")).andExpect(model().errorCount(1)).andExpect(status().isOk());
 
-		// TODO add users
 		mockMvc.perform(post("/signup").param("email", "test2@mail.com").param("password", "1234"))
 				.andExpect(view().name("user/signup")).andExpect(model().errorCount(0)).andExpect(status().isOk());
+
+		mockMvc.perform(post("/signup").param("email", "test2@mail.com").param("password", "1234"))
+				.andExpect(view().name("user/signup")).andExpect(model().errorCount(1)).andExpect(status().isOk());
 
 		MvcResult result = mockMvc.perform(get("/home/connection")).andExpect(view().name("connection/connection"))
 				.andExpect(model().errorCount(0)).andExpect(status().isOk()).andReturn();
@@ -46,19 +54,29 @@ public class BaseDataTest {
 		System.out.println("Content" + content);
 
 		// TODO check the Html in content contains
-
+		
 		// <p class="pstyle_account"> Email: <span style="font-weight:
 		// 700;">test@mail.com</span></p>
 		// <p class="pstyle_account"> Balance: <span style="font-weight:
 		// 700;">100.00</span></p>
 	}
 
-	protected void testAddConnection() {
+	protected void testAddConnection() throws Exception {
 		// TODO add connections
+		mockMvc.perform(post("/home/connection").param("email", "test@mail.com"))
+				.andExpect(view().name("redirect:/home/connection/")).andExpect(model().errorCount(0))
+				.andExpect(status().isFound());
+
+		mockMvc.perform(post("/home/connection").param("email", "test2@mail.com"))
+				.andExpect(view().name("redirect:/home/connection/")).andExpect(model().errorCount(0))
+				.andExpect(status().isFound());
+		
+		mockMvc.perform(post("/home/connection")).andExpect(view().name("redirect:/home/connection/"))
+				.andExpect(model().errorCount(0)).andExpect(status().isFound()).andReturn();
 	}
 
 	protected void testUserRepository() {
-
+		
 	}
 
 	protected void testConnectionrRepository() {
--- src/test/java/com/opneclassrooms/paymybuddy/ConnectionControllerTest.java
+++ src/test/java/com/opneclassrooms/paymybuddy/ConnectionControllerTest.java
@@ -1,25 +1,12 @@
 package com.opneclassrooms.paymybuddy;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.openclassrooms.paymybuddy.Application;
 
 @RunWith(SpringRunner.class)
