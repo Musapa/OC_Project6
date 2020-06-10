23fa84b5a419c5060c16a2ed0a25616d10360267
toncy11 <ajagodic1990@gmail.com>
2020-06-10 17:09:14 +0200

    clearing database after each test

--- src/test/java/com/opneclassrooms/paymybuddy/UserControllerTest.java
+++ src/test/java/com/opneclassrooms/paymybuddy/UserControllerTest.java
@@ -2,8 +2,12 @@ package com.opneclassrooms.paymybuddy;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -13,8 +17,10 @@ import com.openclassrooms.paymybuddy.Application;
 @SpringBootTest(classes = Application.class)
 @ActiveProfiles("test")
 @WithMockUser(username = "test@mail.com", roles = { "ADMIN" })
+@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
+@AutoConfigureTestDatabase(replace = Replace.ANY)
 public class UserControllerTest extends BaseDataTest {
-
+	
 	@Test
 	public void testRegistration() throws Exception {
 		super.testRegisterUser();
